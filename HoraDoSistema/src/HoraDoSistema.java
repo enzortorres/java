822e7976bf075875bfb65717dfb88fca3033be9f
Enzo Ribas <enzortorres2005@gmail.com>
2024-12-10 21:51:47 -0300

    studying java

--- HoraDoSistema/src/HoraDoSistema.java
+++ HoraDoSistema/src/HoraDoSistema.java
@@ -3,7 +3,6 @@ import java.util.Date;
 public class HoraDoSistema {
 	public static void main(String[] args) {
 		Date date = new Date();
-		System.out.println("A hora do sistema é: ");
-		System.out.println(date.toString());
+		System.out.println("A hora do sistema é: " + date.toString());
 	}
 }
