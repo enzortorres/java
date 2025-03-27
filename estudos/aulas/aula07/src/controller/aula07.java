d744bc5bc8245b98e3b4b1ea4696a9b242e49225
enzortorres <enzortorres2005@gmail.com>
2025-03-27 19:40:47 -0300

    aula 07

---
+++ estudos/aulas/aula07/src/controller/aula07.java
@@ -0,0 +1,15 @@
+package controller;
+import java.util.ArrayList;
+
+public class aula07 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        for (int i = 0; i < 3;i++) {
+            numbers2.add(i);
+        }
+        for (int i = 0; i < 3;i++) {
+        }
+        System.out.println(numbers2.get(i));
+    }
+}
