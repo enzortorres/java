4adfa2db2f3d46f4a1e00a217ffd207c32256b28
enzortorres <enzortorres2005@gmail.com>
2025-09-09 21:34:57 -0300

    aula 09/09 swing

--- estudos/faculdade/colecoes/lista/arrayList/aula02/src/view/Time.java
+++ estudos/faculdade/colecoes/lista/arrayList/aula02/src/view/Time.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Time extends JFrame {
 
@@ -37,6 +39,20 @@ public class Time extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("CADASTRO DE TIME\r\n");
+		lblNewLabel.setBounds(149, 10, 98, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("SIGLA:\r\n");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(52, 56, 92, 26);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("DESCRIÇÃO:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(52, 79, 92, 26);
+		contentPane.add(lblNewLabel_2);
 	}
-
 }
